77f899f97917e088259bd0bd89254ea7dadf25b2
yaminisiripuram97 <yamini.siripuram97@gmail.com>
2023-08-22 10:50:53 +0530

    this project is about micro contoller

---
+++ test_bench.v
@@ -0,0 +1,24 @@
+
+
+Verilog testbench code to verify the microcontroller: 
+module MCU_tb;
+
+reg clk;
+reg rst;
+// Instantiate the Unit Under Test (UUT)
+MicroController uut (
+.clk(clk), 
+.rst(rst)
+);
+initial begin
+// Initialize Inputs
+rst = 1;
+// Wait 100 ns for global reset to finish
+#100;
+rst = 0;
+end
+initial begin 
+clk = 0;
+forever #10 clk = ~clk;
+end 
+endmodule 
